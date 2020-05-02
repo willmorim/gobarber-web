28a1fb25c7e5e7c9e104c8cc8706393fac9085f8
willmorim <pdbawilliam@gmail.com>
2020-05-02 15:39:21 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>oi william</h1>;
+import GlobalStyle from './styles/globals';
+
+const App: React.FC = () => (
+  <>
+    <h1>oi william</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
