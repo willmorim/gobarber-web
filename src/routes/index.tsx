04ea7e86c5e56d1fc477dddb514c09a55506f6a4
willmorim <pdbawilliam@gmail.com>
2020-08-06 23:45:41 -0300

    Criando página de redefinição

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,12 +7,14 @@ import ForgotPassword from '../pages/ForgotPassword';
 import Dashboard from '../pages/Dashboard';
 
 import Route from './Route';
+import ResetPassword from '../pages/ResetPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
     <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
