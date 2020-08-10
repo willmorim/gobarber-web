7e695c8320b42b2f63f6dd972b53b5f811cd5cfe
willmorim <pdbawilliam@gmail.com>
2020-08-09 23:02:55 -0300

    Disponibilidade do mês

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -30,6 +30,7 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@Gobarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
 
@@ -45,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@Gobarber:token', token);
     localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
